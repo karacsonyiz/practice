841255d26240f03cac5b9a3f6564ed56f61dc73b
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-04-04 18:40:38 +0200

    test commit

---
+++ packages/src/main/java/packages/TrainerMain.java
@@ -0,0 +1,15 @@
+package packages;
+
+import main.Trainer;
+
+public class TrainerMain {
+
+    public static void main(String[]args){
+
+        Trainer trainer = new Trainer("Mr Trainer");
+        System.out.println(trainer.getName());
+    }
+}
+
+
+
