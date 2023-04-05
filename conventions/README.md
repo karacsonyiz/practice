ac48c2db2ede9af21acb9fe158a1b0808d792ae0
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-04-05 18:02:15 +0200

    conventions task

---
+++ conventions/README.md
@@ -0,0 +1,3 @@
+Kódolási konvenciók
+
+Formázd meg a letöltött projektben található Book osztályt, hogy megfeleljen a kódolási konvencióknak.
--- conventions/src/main/java/classstructure/Book.java
+++ conventions/src/main/java/classstructure/Book.java
@@ -1,4 +1,29 @@
-package classstructure;public class Book{public String getAuthor(){return author;}public String getTitle(){return
-title;}public String getRegNumber(){return regNumber;}public void register(String regNumber){this.regNumber=regNumber;
-}public Book(String author,String title) {this.author=author;this.title=title;}private String author;private String
-title;private String regNumber;}
+package classstructure;
+
+public class Book {
+
+    private String author;
+    private String title;
+    private String regNumber;
+
+    public Book(String author,String title) {
+        this.author=author;
+        this.title=title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getRegNumber(){
+        return regNumber;
+    }
+
+    public void register(String regNumber){
+        this.regNumber=regNumber;
+    }
+}
