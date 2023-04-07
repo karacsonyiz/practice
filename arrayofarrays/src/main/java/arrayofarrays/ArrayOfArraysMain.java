fed05ec9c21d98ee7cf520199b1e4cbced4487f8
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-04-07 19:55:29 +0200

    arrayofarrays task#2,task#3

--- arrayofarrays/src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ arrayofarrays/src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -6,8 +6,10 @@ public class ArrayOfArraysMain {
 
     public static void main(String[] args) {
         ArrayOfArraysMain arrayOfArraysMain = new ArrayOfArraysMain();
-        int [][] result = arrayOfArraysMain.multiplicationTable(5);
+        int [][] result = arrayOfArraysMain.multiplicationTable(3);
+        arrayOfArraysMain.printArrayOfArrays(result);
         System.out.println(Arrays.deepToString(result));
+        arrayOfArraysMain.triangularMatrix(3);
     }
 
     public int[][] multiplicationTable(int size){
@@ -18,5 +20,27 @@ public class ArrayOfArraysMain {
             }
         }
         return table;
+    }
+
+    public void printArrayOfArrays(int[][] a){
+       for(int i=0;i<a.length;i++){
+           for(int j = 0;j<a.length;j++){
+               System.out.print(a[i][j] + " ");
+           }
+           System.out.println();
+       }
+    }
+
+    public int[][] triangularMatrix(int size){
+        int [][] arr = new int[size+1][];
+        for(int i = 0;i < size+1;i++){
+            arr[i] = new int[i+1];
+            for(int j = 0;j < arr[i].length;j++){
+                arr[i][j] = i;
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        return arr;
     }
 }
