67b922cda71f8a088043e96f3ac781babc0c874c
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-05-01 20:59:48 +0200

    methodparam

---
+++ methodparam/src/main/java/methodparam/measuring/ExtremValues.java
@@ -0,0 +1,20 @@
+package methodparam.measuring;
+
+public class ExtremValues {
+
+    public double min;
+    public double max;
+
+    public ExtremValues(double min, double max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public double getMax() {
+        return max;
+    }
+}
---
+++ methodparam/src/main/java/methodparam/measuring/Measurement.java
@@ -0,0 +1,44 @@
+package methodparam.measuring;
+
+public class Measurement {
+
+    private double[] values;
+
+    public Measurement(double[] values) {
+        this.values = values;
+    }
+
+    public int findFirstIndexInLimit(int lower, int upper) {
+
+        for(int i = 0; i < values.length; i++){
+            if(values[i] > lower && values[i] < upper)
+                return i;
+        }
+        return -1;
+
+    }
+
+    public double minimum() {
+        double min = values[0];
+        for(int i = 0; i < values.length;i++) {
+            if(values[i] < min){
+                min = values[i];
+            }
+        }
+        return min;
+    }
+
+    public double maximum() {
+        double max = values[0];
+        for(int i = 0; i < values.length;i++) {
+            if(values[i] > max){
+                max = values[i];
+            }
+        }
+        return max;
+    }
+
+    public ExtremValues minmax() {
+        return new ExtremValues(minimum(),maximum());
+    }
+}
