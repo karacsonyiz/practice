34087c587b2dd42aba357b6434ed31ffb4145cd1
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-07-14 20:00:02 +0200

    generics

---
+++ genericsusage/src/main/java/genericsusage/withgenerics/Library.java
@@ -0,0 +1,15 @@
+package genericsusage.withgenerics;
+
+import genericsusage.Book;
+
+import java.util.List;
+
+public class Library {
+
+    public Book getFirstBook(List<Book> bookList){
+        if(bookList.isEmpty()) {
+            throw new IllegalArgumentException("Argument should not be empty");
+        }
+        return bookList.get(0);
+    }
+}
---
+++ genericsusage/src/main/java/genericsusage/withoutgenerics/Library.java
@@ -0,0 +1,21 @@
+package genericsusage.withoutgenerics;
+
+import genericsusage.Book;
+import java.util.List;
+
+public class Library {
+
+    public Book getFirstBook(List bookList){
+        if(bookList.isEmpty()) {
+            throw new IllegalArgumentException("Argument should not be empty");
+        }
+        Book book;
+        try {
+            book = (Book) bookList.get(0);
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Not a book");
+        }
+        return book;
+    }
+
+}
