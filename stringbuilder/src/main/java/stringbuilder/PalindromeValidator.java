2a63ee0d3e3fdff5d4c23b72b568378fc925c03a
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-08-04 21:21:16 +0200

    stringbuilder #5

--- stringbuilder/src/main/java/stringbuilder/PalindromeValidator.java
+++ stringbuilder/src/main/java/stringbuilder/PalindromeValidator.java
@@ -3,9 +3,13 @@ package stringbuilder;
 public class PalindromeValidator {
 
     public boolean isPalindrome(String text) {
-
         if(text == null) {
             throw new IllegalArgumentException("Text must not be null!");
+        }
+
+        StringBuilder sb = new StringBuilder(text.toLowerCase().replace("\n\t",""));
+        if(sb.toString().equals(sb.reverse().toString())){
+            return true;
         }
         return false;
     }
