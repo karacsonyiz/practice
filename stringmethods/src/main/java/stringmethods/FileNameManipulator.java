f239f645bf9800e7482606f692f59d801ab71a6d
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-08-06 23:15:20 +0200

    stringmethods #1

---
+++ stringmethods/src/main/java/stringmethods/FileNameManipulator.java
@@ -0,0 +1,14 @@
+package stringmethods;
+
+public class FileNameManipulator {
+
+    public String findLastCharacter(String word){
+
+        if(word.isEmpty()) {
+            throw new IllegalArgumentException("Empty string!");
+        }
+
+        return null;
+
+    }
+}
