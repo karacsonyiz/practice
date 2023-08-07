535dd9e8632ab4c3d12b4dcd074b9b198fce9a22
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-08-07 23:45:07 +0200

    stringmethods #2

--- stringmethods/src/main/java/stringmethods/FileNameManipulator.java
+++ stringmethods/src/main/java/stringmethods/FileNameManipulator.java
@@ -8,7 +8,26 @@ public class FileNameManipulator {
             throw new IllegalArgumentException("Empty string!");
         }
 
-        return null;
+        return word.substring(word.length()-1);
 
+    }
+
+    public String findFileExtension(String fileName){
+        if(fileName.isEmpty()) {
+            throw new IllegalArgumentException("Empty string!");
+        }
+        return null;
+    }
+    public boolean identifyFilesByExtension(String ext, String fileName){
+        return false;
+    }
+    public boolean compareFilesByName(String searchedFileName, String actualFileName){
+        return false;
+    }
+    public String changeExtensionToLowerCase(String fileName){
+        return null;
+    }
+    public String replaceStringPart(String fileName, String present, String target){
+        return null;
     }
 }
