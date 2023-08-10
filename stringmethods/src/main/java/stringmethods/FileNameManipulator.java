3e9a51d02a3df8cbc70608bfbd15f9e36783be99
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-08-10 16:49:29 +0200

    stringmethods #4

--- stringmethods/src/main/java/stringmethods/FileNameManipulator.java
+++ stringmethods/src/main/java/stringmethods/FileNameManipulator.java
@@ -17,21 +17,65 @@ public class FileNameManipulator {
             throw new IllegalArgumentException("Empty string!");
         }
 
+        if(fileName.split("\\.")[0].isEmpty()){
+            throw new IllegalArgumentException("Invalid file name!");
+        }
+
         return "." + fileName.split("\\.")[1];
     }
     public boolean identifyFilesByExtension(String ext, String fileName){
+
+        if(fileName.split("\\.")[0].isEmpty()){
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+
+        if(fileName.split("\\.").length != 2){
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+
+        String  trimmed = ext.trim();
+
+        if(trimmed.isEmpty()){
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+
         if(ext.equals(fileName.split("\\.")[1])){
             return true;
         }
         return false;
     }
     public boolean compareFilesByName(String searchedFileName, String actualFileName){
+
+        if(searchedFileName == null){
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+
+
+
+        String  trimmed = searchedFileName.trim();
+
+        if(trimmed.isEmpty()){
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+
+
         if(searchedFileName.toLowerCase().equals(actualFileName.toLowerCase())){
             return true;
         }
         return false;
     }
     public String changeExtensionToLowerCase(String fileName){
+
+        String  trimmed = fileName.trim();
+
+        if(trimmed.isEmpty()){
+            throw new IllegalArgumentException("Empty string!");
+        }
+
+
+        if(fileName.split("\\.").length != 2){
+            throw new IllegalArgumentException("Invalid argument!");
+        }
 
         String file = fileName.split("\\.")[0];
         String ext = fileName.split("\\.")[1].toLowerCase();
@@ -40,8 +84,13 @@ public class FileNameManipulator {
     }
     public String replaceStringPart(String fileName, String present, String target){
 
-        String file = fileName.split("\\.")[0];
+        String  trimmed = fileName.trim();
 
-        return null;
+        if(trimmed.isEmpty()){
+            throw new IllegalArgumentException("Empty string!");
+        }
+
+        return fileName.replaceAll(present,target);
+
     }
 }
