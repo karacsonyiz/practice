defafe66eeb5cd12ba7e569ed0506596898f523c
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-08-15 18:50:59 +0200

    alorithmsmax #4

---
+++ operators/src/main/java/operators/OperatorsMain.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class OperatorsMain {
+
+    public static void main(String[] args) {
+        Operators op = new Operators();
+
+        System.out.println(op.isEven(5));
+        System.out.println(op.isEven(10));
+
+    }
+}
