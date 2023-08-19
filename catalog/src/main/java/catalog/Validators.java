814df0899914e11268ce065813475c745700aa21
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-08-19 15:14:52 +0200

    catalog #4 Validators fix

--- catalog/src/main/java/catalog/Validators.java
+++ catalog/src/main/java/catalog/Validators.java
@@ -5,10 +5,16 @@ import java.util.List;
 public class Validators {
 
     public static boolean isBlank(String text){
-        return true;
+        if(text == null || text.trim().isEmpty()){
+            return true;
+        }
+        return false;
     }
 
     public static boolean isEmpty(List<String> texts){
-        return true;
+        if(texts == null || texts.size() == 0){
+            return true;
+        }
+        return false;
     }
 }
