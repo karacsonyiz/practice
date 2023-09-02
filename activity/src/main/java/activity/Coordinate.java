b6e3e633bb096c4de78a9c8da99f409e9ecef4e0
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-09-02 17:55:26 +0200

    Activity #3 Coordinate error check

--- activity/src/main/java/activity/Coordinate.java
+++ activity/src/main/java/activity/Coordinate.java
@@ -2,10 +2,11 @@ package activity;
 
 public class Coordinate {
 
-    private double latitude;
-    private double longitude;
+    private final double latitude;
+    private final double longitude;
 
     public Coordinate(double latitude, double longitude) {
+        checkIfCoordinatesAreValid(latitude,longitude);
         this.latitude = latitude;
         this.longitude = longitude;
     }
@@ -16,5 +17,14 @@ public class Coordinate {
 
     public double getLongitude() {
         return longitude;
+    }
+
+    private void checkIfCoordinatesAreValid(double lat, double lon){
+        if(lat > 180 || lat < -180) {
+            throw new IllegalArgumentException();
+        }
+        if(lon > 90 || lon < -90) {
+            throw new IllegalArgumentException();
+        }
     }
 }
