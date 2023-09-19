a6e195827dd7fdfeff6264be9266517b1b21e71d
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-09-19 20:34:09 +0200

    Controladvanced #1 find implemented with CollectionUtils

--- controladvanced/src/main/java/controladvanced/duplicates/Duplicates.java
+++ controladvanced/src/main/java/controladvanced/duplicates/Duplicates.java
@@ -1,10 +1,12 @@
 package controladvanced.duplicates;
 
-import java.util.List;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.*;
 
 public class Duplicates {
 
     public List<Integer> find(List<Integer> integers){
-        return null;
+        return new ArrayList<Integer>(CollectionUtils.disjunction(integers, new HashSet<>(integers)));
     }
 }
