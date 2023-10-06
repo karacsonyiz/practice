0bc95a8e06397fdbd828ff74eb0383ddf8b57852
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-10-06 08:10:29 +0200

    Controlselection #6 Greetings fix

--- controlselection/src/main/java/controlselection/greetings/Greetings.java
+++ controlselection/src/main/java/controlselection/greetings/Greetings.java
@@ -5,26 +5,24 @@ public class Greetings {
     public String greet(int hour,int minute){
 
         String greet = "";
-
         int timeInMinutes = (hour * 60) + minute;
 
-        switch (timeInMinutes) {
-            case 540:
-                greet = "jó reggelt";
-                break;
-            case 1110:
-                greet = "jó napot";
-                break;
-            case 1200:
-                greet = "jó estét";
-                break;
-            case 1201:
-                greet = "jó éjt";
-                break;
+        if(timeInMinutes <= 300 || timeInMinutes > 1200){
+            greet = "jó éjt";
         }
+
+        if(timeInMinutes > 300 && timeInMinutes <= 540){
+            greet = "jó reggelt";
+        }
+
+        if(timeInMinutes > 540 && timeInMinutes <= 1110){
+            greet = "jó napot";
+        }
+
+        if(timeInMinutes > 1110 && timeInMinutes <= 1200){
+            greet = "jó estét";
+        }
+
         return greet;
-
-
-
     }
 }
