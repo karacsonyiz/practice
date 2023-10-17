968791a9a6f54d08ffba824afbc5a79b00d15c8a
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-10-17 09:30:07 +0200

    GenericClass #1 ArraySearch Validation

--- genericclass/src/main/java/genericclass/ArraySearch.java
+++ genericclass/src/main/java/genericclass/ArraySearch.java
@@ -3,7 +3,19 @@ package genericclass;
 public class ArraySearch {
 
     public DataPair<String> getFirstAndLastWord(String[] words){
+        checkIfValid(words);
         return null;
+    }
+
+
+
+    private void checkIfValid(String[] words){
+        if(words == null){
+            throw new NullPointerException();
+        }
+        if(words.length == 0){
+            throw new IllegalArgumentException();
+        }
     }
 
 }
