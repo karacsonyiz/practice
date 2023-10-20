61d5d888dde106b8f8cba44768a00b989c47490d
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-10-20 10:56:35 +0200

    Polymorphism #2 Tests 2

--- polymorphism/src/test/java/polymorphism/TrainerTest.java
+++ polymorphism/src/test/java/polymorphism/TrainerTest.java
@@ -3,9 +3,7 @@ package polymorphism;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
@@ -18,12 +16,28 @@ public class TrainerTest {
     @Test
     public void testCreate(){
         Trainer trainer = new Trainer("John Doe", Arrays.asList(new Course("Course1")));
-
         assertThat(trainer, instanceOf(Trainer.class));
         assertThat(trainer, instanceOf(Human.class));
         assertThat(trainer, instanceOf(Object.class));
         assertThat(trainer, instanceOf(HasName.class));
 
+        Object object = new Trainer("John Doe", Arrays.asList(new Course("Course1")));
+        assertThat(object, instanceOf(Trainer.class));
+        assertThat(object, instanceOf(Human.class));
+        assertThat(object, instanceOf(Object.class));
+        assertThat(object, instanceOf(HasName.class));
+
+        Human human = new Trainer("John Doe", Arrays.asList(new Course("Course1")));
+        assertThat(human, instanceOf(Trainer.class));
+        assertThat(human, instanceOf(Human.class));
+        assertThat(human, instanceOf(Object.class));
+        assertThat(human, instanceOf(HasName.class));
+
+        HasName hasName = new Trainer("John Doe", Arrays.asList(new Course("Course1")));
+        assertThat(hasName, instanceOf(Trainer.class));
+        assertThat(hasName, instanceOf(Human.class));
+        assertThat(hasName, instanceOf(Object.class));
+        assertThat(hasName, instanceOf(HasName.class));
 
     }
 
