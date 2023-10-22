8c3276ecfbb21468f2e08e57d9d5501afbff6ec9
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-10-22 23:09:23 +0200

    server pom.xml fix + tests

---
+++ server/src/main/java/server/BooksApplication.java
@@ -0,0 +1,34 @@
+package server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class BooksApplication {
+
+    private List<Book> books = Arrays.asList(
+            new Book("Szophoklész","Antigoné"),
+            new Book("Dickens, Charles","Karácsonyi ének"),
+            new Book("Gárdonyi Géza","Egri csillagok"));
+
+    public static void main(String[] args) { SpringApplication.run(BooksApplication.class,args); }
+
+    @RequestMapping("/books")
+    public List<Book> books(){
+        return books;
+    }
+
+    @RequestMapping("/book/{id}")
+    public Book book(@PathVariable int id){
+        return books.get(id);
+    }
+
+}
