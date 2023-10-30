2cbc644092c568e0bf0a8551cc2a6ed46c913ba4
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-10-30 22:21:00 +0100

    small fix, application.properties added

--- server/src/main/java/server/BooksApplication.java
+++ server/src/main/java/server/BooksApplication.java
@@ -1,11 +1,10 @@
 package server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BooksApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BooksApplication.class,args);
     }}
