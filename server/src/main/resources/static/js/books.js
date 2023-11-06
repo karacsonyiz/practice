53bb55eef1fc341796c543e259dbe6fc361a482b
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-11-06 14:47:00 +0100

    books js refactored to modern js instead of concatenation

--- server/src/main/resources/static/js/books.js
+++ server/src/main/resources/static/js/books.js
@@ -4,7 +4,6 @@ window.onload = function(){
 }
 
 function createBook(){
-
     let author = document.querySelector("#authorInput").value;
     let title = document.querySelector("#titleInput").value;
 
@@ -29,19 +28,45 @@ function createBook(){
 }
 
 function getBooks() {
-        var booksTable = document.querySelector("#booksTable");
-        booksTable.innerHTML = "";
         fetch("/books")
             .then(function(response) {
                 return response.json();
             })
             .then(function(jsonData) {
-                for(let i = 0;i < jsonData.length; i++){
-                    let btn = document.createElement("button");
-                    btn.setAttribute("id",jsonData[i].id);
-                    booksTable.innerHTML += "<tr><td><a href='book.html?id=" + jsonData[i].id +"'>"+ jsonData[i].author + "</a></td><td>" + jsonData[i].title +
-                    "  <button class='edit' id="+ jsonData[i].id +">Szerkeszt</button><button class='delete' id="+ jsonData[i].id +">Töröl</button></td></tr>";
-                }
+                fillTable(jsonData);
             }).catch(error => console.log(error));
+}
+
+function fillTable(jsonData){
+        const booksTable = document.querySelector("#booksTable");
+        booksTable.innerHTML = "";
+        for(let i = 0;i < jsonData.length; i++){
+            tr = createRow(jsonData[i]);
+            booksTable.appendChild(tr);
+        }
+}
+
+function createRow(row){
+        let tr = document.createElement("tr");
+        let authorTd = document.createElement("td");
+        let link = document.createElement("a");
+        link.setAttribute("href","book.html?id="+row.id);
+        link.innerHTML = row.author;
+        authorTd.appendChild(link);
+        tr.appendChild(authorTd);
+        let titleTd = document.createElement("td");
+        titleTd.innerHTML = row.title;
+        tr.appendChild(titleTd);
+        let deleteBtn = document.createElement("button");
+        deleteBtn.setAttribute("id",row.id);
+        deleteBtn.setAttribute("class","delete");
+        deleteBtn.innerHTML = "Töröl";
+        tr.appendChild(deleteBtn);
+        let editBtn = document.createElement("button");
+        editBtn.setAttribute("id",row.id);
+        editBtn.setAttribute("class","edit");
+        editBtn.innerHTML = "Módosít";
+        tr.appendChild(editBtn);
+        return tr;
 }
 
