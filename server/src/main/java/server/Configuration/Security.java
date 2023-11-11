2274b8ccb020939ea4f85fbd8a80849ebcd41392
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-11-11 16:28:15 +0100

    Deprecated security configuration fix

--- server/src/main/java/server/Configuration/Security.java
+++ server/src/main/java/server/Configuration/Security.java
@@ -1,41 +1,69 @@
 package server.Configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class Security extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class Security {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/js/**", "/css/**", "/api/**", "/WEB-INF/jsp/**").permitAll()
-                .antMatchers("/admin.html").hasRole("ADMIN")
-                .and()
-                .formLogin()
-                .loginPage("/WEB-INF/jsp/login.jsp");
+    @Bean
+    public UserDetailsService jdbcUserDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
-    public PasswordEncoder encoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource, PasswordEncoder passwordEncoder) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder)
-                .usersByUsernameQuery("select name, password, enabled from users where name=?")
-                .authoritiesByUsernameQuery("select name, role from users where name=?");
-    }
-}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http.authorizeRequests()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/**").authenticated()
+                .and()
+                .formLogin().permitAll();
+
+        http.authorizeRequests()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/process-login")
+                .defaultSuccessUrl("/home")
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/resources/**", "/static/**");
+    }
+
+}
