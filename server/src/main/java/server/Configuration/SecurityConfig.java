1b8c479cfb849f5888edd209e21b46103b0d13f4
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-11-14 17:51:13 +0100

    Security config fix, new custom login page

--- server/src/main/java/server/Configuration/SecurityConfig.java
+++ server/src/main/java/server/Configuration/SecurityConfig.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
@@ -27,6 +31,34 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests ()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/js/**", "/css/**").permitAll()
+                .requestMatchers("/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/process-login")
+                .defaultSuccessUrl("/index.html",true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout=true")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+
+        return http.build();
     }
 
 }
