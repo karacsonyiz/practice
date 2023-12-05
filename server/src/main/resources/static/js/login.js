d2a9d0bb18effc4a14f4548196039bbf30ae49c9
karacsonyiz <zoltankaracsonyi91@gmail.com>
2023-12-05 23:32:06 +0100

    autologin added for dev - pw fix

--- server/src/main/resources/static/js/login.js
+++ server/src/main/resources/static/js/login.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 document.querySelector("#username").value = "admin";
-document.querySelector("#password").value = "admin";
+document.querySelector("#password").value = "$2a$10$FV9w7BuvK2gmnVPaUqQ88.d9V2C73VyTvc24DrqHZJXTGnQV5xtl.";
 
 document.querySelector('#loginbutton').click();
 }
