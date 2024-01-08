6ce385b7941a2f1d1c643354651ab6d234de653d
karacsonyiz <zoltankaracsonyi91@gmail.com>
2024-01-08 22:20:31 +0100

    basket service, controller added

---
+++ server/src/main/java/server/Controller/BasketController.java
@@ -0,0 +1,25 @@
+package server.Controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.Entity.Basket;
+import server.Service.BasketService;
+
+@RestController
+public class BasketController {
+
+    private BasketService basketService;
+
+    public BasketController(BasketService basketService) {
+        this.basketService = basketService;
+    }
+
+    @RequestMapping("/basket/{id}")
+    public Basket getBasket(@PathVariable Long id){
+        if(basketService.getBasketById(id).isPresent()){
+            return basketService.getBasketById(id).get();
+        }
+        return null;
+    }
+}
---
+++ server/src/main/java/server/Service/BasketService.java
@@ -0,0 +1,22 @@
+package server.Service;
+
+
+import org.springframework.stereotype.Service;
+import server.Database.BasketRepository;
+import server.Entity.Basket;
+
+import java.util.Optional;
+
+@Service
+public class BasketService {
+
+    private BasketRepository basketRepository;
+
+    public BasketService(BasketRepository basketRepository) {
+        this.basketRepository = basketRepository;
+    }
+
+    public Optional<Basket> getBasketById(Long id){
+        return basketRepository.findById(id);
+    }
+}
