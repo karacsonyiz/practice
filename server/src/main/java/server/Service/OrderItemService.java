633bd1e364a4ba5aa3a76800fa8be1a8a102701a
karacsonyiz <zoltankaracsonyi91@gmail.com>
2024-01-23 22:24:22 +0100

    order,orderitem findbyIs, delete added

--- server/src/main/java/server/Service/OrderItemService.java
+++ server/src/main/java/server/Service/OrderItemService.java
@@ -3,14 +3,16 @@ package server.Service;
 
 import org.springframework.stereotype.Service;
 import server.Database.OrderItemRepository;
+import server.Entity.Order;
 import server.Entity.OrderItem;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderItemService {
 
-    private OrderItemRepository orderItemRepository;
+    private final OrderItemRepository orderItemRepository;
 
     public OrderItemService(OrderItemRepository orderItemRepository) {
         this.orderItemRepository = orderItemRepository;
@@ -18,5 +20,14 @@ public class OrderItemService {
 
     public List<OrderItem> getOrderItems() {
         return orderItemRepository.findAll();
+    }
+
+    public Optional<OrderItem> getOrderItemById(Long id){
+        return orderItemRepository.findById(id);
+    }
+
+    public Long deleteOrderItem(Long id){
+        orderItemRepository.deleteById(id);
+        return id;
     }
 }
--- server/src/main/java/server/Service/OrderService.java
+++ server/src/main/java/server/Service/OrderService.java
@@ -2,14 +2,16 @@ package server.Service;
 
 import org.springframework.stereotype.Service;
 import server.Database.OrderRepository;
+import server.Entity.Book;
 import server.Entity.Order;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
 
-    private OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
 
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -22,5 +24,15 @@ public class OrderService {
     public long createOrder(Order order) {
         orderRepository.save(order);
         return order.getId();
+    }
+
+    public Optional<Order> getOrderById(Long id){
+        return orderRepository.findById(id);
+    }
+
+
+    public Long deleteOrder(Long id){
+        orderRepository.deleteById(id);
+        return id;
     }
 }
