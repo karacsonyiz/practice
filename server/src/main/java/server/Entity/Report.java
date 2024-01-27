4a638325dc97664ce6ebf5a5de865125eb727304
karacsonyiz <zoltankaracsonyi91@gmail.com>
2024-01-27 23:56:02 +0100

    report entity, sql added

---
+++ server/src/main/java/server/Entity/Report.java
@@ -0,0 +1,76 @@
+package server.Entity;
+
+public class Report {
+
+    private long totalPrice;
+    private String date;
+    private int month;
+    private OrderStatus status;
+    private String productName;
+    private long productCount;
+    private long productPrice;
+
+    public Report(String productName,int month,long productCount,long productPrice,long totalPrice) {
+        this.month = month;
+        this.productName = productName;
+        this.productCount = productCount;
+        this.productPrice = productPrice;
+        this.totalPrice = totalPrice;
+    }
+
+    public long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(long totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public long getProductCount() {
+        return productCount;
+    }
+
+    public void setProductCount(long productCount) {
+        this.productCount = productCount;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public long getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(long productPrice) {
+        this.productPrice = productPrice;
+    }
+}
