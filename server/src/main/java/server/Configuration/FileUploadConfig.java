544d4bb7dfc7304c8c5865de61989c2aa2181dcf
karacsonyiz <zoltankaracsonyi91@gmail.com>
2024-02-04 00:04:50 +0100

    File Upload config fix

--- server/src/main/java/server/Configuration/FileUploadConfig.java
+++ server/src/main/java/server/Configuration/FileUploadConfig.java
@@ -1,16 +1,17 @@
 package server.Configuration;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 @Configuration
 public class FileUploadConfig {
 
     @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(500000);
+    public StandardServletMultipartResolver multipartResolver(HttpServletRequest request) {
+        StandardServletMultipartResolver multipartResolver = new StandardServletMultipartResolver();
+        multipartResolver.resolveMultipart(request);
         return multipartResolver;
     }
 }
