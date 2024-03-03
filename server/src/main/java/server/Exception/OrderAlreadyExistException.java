8d477c109d7d6ab2b6a13f42bb733dd38d9bda86
karacsonyiz <zoltankaracsonyi91@gmail.com>
2024-03-03 23:59:39 +0100

    order already exist exception fix

--- server/src/main/java/server/Exception/OrderAlreadyExistException.java
+++ server/src/main/java/server/Exception/OrderAlreadyExistException.java
@@ -1,6 +1,6 @@
 package server.Exception;
 
-public class OrderAlreadyExistException extends RuntimeException {
+public class OrderAlreadyExistsException extends RuntimeException {
 
     public OrderAlreadyExistException(String message) {
         super(message);
