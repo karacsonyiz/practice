9cd200ff2a2d45382e765f5c1d5f3c1d9db11288
karacsonyiz <zoltankaracsonyi91@gmail.com>
2024-03-14 23:16:52 +0100

    Dashboard controller init

---
+++ server/src/main/java/server/Controller/DashboardController.java
@@ -0,0 +1,23 @@
+package server.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import server.Pojo.Dashboard;
+import server.Service.DashboardService;
+
+@RestController
+public class DashboardController {
+
+    private DashboardService dashboardService;
+
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    public Dashboard createDashboard() {
+        return dashboardService.createDashboard();
+    }
+
+}
