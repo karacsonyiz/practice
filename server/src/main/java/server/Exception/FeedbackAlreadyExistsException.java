d6ad10c3c20031a01a2b239a3997bc4ec4778fe9
Zoli <zoltankaracsonyi91@gmail.com>
2024-03-18 23:56:14 +0100

    Feedbackalreadyexists exceptiona added

---
+++ server/src/main/java/server/Exception/FeedbackAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package server.Exception;
+
+public class FeedbackAlreadyExistsException extends RuntimeException {
+
+    public FeedbackAlreadyExistsException(String message) {
+        super(message);
+    }
+}
