004d14902f9fe551665a4b9b21f279d138d41ccd
Zoli <zoltankaracsonyi91@gmail.com>
2024-04-09 23:41:17 +0200

    report mock data

--- server/src/main/resources/db/migration/V6__report.sql
+++ server/src/main/resources/db/migration/V6__report.sql
@@ -6,3 +6,11 @@ product_count int NOT NULL,
 product_price int NOT NULL,
 total_price int NOT NULL
 );
+
+ INSERT INTO report
+(month, product_name, product_count, product_price, total_price)
+VALUES(2, "test1", 2, 100, 200),
+(5, "test2", 1, 50, 50),
+(8, "test3", 5, 200, 1000),
+(1, "test4", 1, 500, 500)
+;
