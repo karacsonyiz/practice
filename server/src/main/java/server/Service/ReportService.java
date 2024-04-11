a3448359c2e4dba0349cf9dce37ad5f263a94890
Zoli <zoltankaracsonyi91@gmail.com>
2024-04-11 23:03:53 +0200

    Reportservice CRUD impl

--- server/src/main/java/server/Service/ReportService.java
+++ server/src/main/java/server/Service/ReportService.java
@@ -7,12 +7,13 @@ import server.Database.ReportRepository;
 import server.Entity.Report;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReportService {
 
     private final ReportRepository reportRepository;
-    public static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+    public static final Logger LOGGER = LoggerFactory.getLogger(ReportService.class);
 
     public ReportService(ReportRepository reportRepository) {
         this.reportRepository = reportRepository;
@@ -20,5 +21,21 @@ public class ReportService {
 
     public List<Report> listReports(){
         return reportRepository.findAll();
+    }
+
+    public Optional<Report> getReportById(int id){
+        return reportRepository.findById(id);
+    }
+
+    public long createReport(Report report){
+        reportRepository.save(report);
+        LOGGER.info("Report Created with id : " + report.getId());
+        return report.getId();
+    }
+
+    public int deleteReport(int id){
+        reportRepository.deleteById(id);
+        LOGGER.info("Report Deleted with id : " + id);
+        return id;
     }
 }
