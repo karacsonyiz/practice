84078d6041459c316277cd6402e174c741a9143d
Zoli <zoltankaracsonyi91@gmail.com>
2024-05-23 23:56:58 +0200

    Lombok NoArgsConstructor fix

--- server/src/main/java/server/Entity/Order.java
+++ server/src/main/java/server/Entity/Order.java
@@ -5,11 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Order {
 
     @Id
