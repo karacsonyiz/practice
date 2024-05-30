100e31b888e7a7c360710f26070e7d16c86b0424
Zoli <zoltankaracsonyi91@gmail.com>
2024-05-30 17:39:14 +0200

    revert lombok in feedback,order,order item

--- server/src/main/java/server/Entity/Feedback.java
+++ server/src/main/java/server/Entity/Feedback.java
@@ -1,14 +1,10 @@
 package server.Entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Data
-@NoArgsConstructor
 @Access(value= AccessType.FIELD)
 public class Feedback {
 
@@ -20,4 +16,64 @@ public class Feedback {
     private LocalDateTime ratingDate;
     private User user;
     private boolean canEditOrDelete;
+
+    public Feedback() {
+    }
+
+    public Feedback(long id, int ratingScore, String ratingText, LocalDateTime ratingDate, User user, boolean canEditOrDelete) {
+        this.id = id;
+        this.ratingScore = ratingScore;
+        this.ratingText = ratingText;
+        this.ratingDate = ratingDate;
+        this.user = user;
+        this.canEditOrDelete = canEditOrDelete;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getRatingScore() {
+        return ratingScore;
+    }
+
+    public void setRatingScore(int ratingScore) {
+        this.ratingScore = ratingScore;
+    }
+
+    public String getRatingText() {
+        return ratingText;
+    }
+
+    public void setRatingText(String ratingText) {
+        this.ratingText = ratingText;
+    }
+
+    public LocalDateTime getRatingDate() {
+        return ratingDate;
+    }
+
+    public void setRatingDate(LocalDateTime ratingDate) {
+        this.ratingDate = ratingDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isCanEditOrDelete() {
+        return canEditOrDelete;
+    }
+
+    public void setCanEditOrDelete(boolean canEditOrDelete) {
+        this.canEditOrDelete = canEditOrDelete;
+    }
 }
--- server/src/main/java/server/Entity/Order.java
+++ server/src/main/java/server/Entity/Order.java
@@ -4,14 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class Order {
 
     @Id
@@ -21,4 +17,55 @@ public class Order {
     private LocalDateTime date;
     private OrderStatus status;
     private String deliveryAddress;
+
+    public Order() {
+    }
+
+    public Order(long id, long userId, LocalDateTime date, OrderStatus status, String deliveryAddress) {
+        this.id = id;
+        this.userId = userId;
+        this.date = date;
+        this.status = status;
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
 }
