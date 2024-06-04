109eb71470ef4dd9e56fd5ccff9ed93cd9c69d01
Zoli <zoltankaracsonyi91@gmail.com>
2024-06-04 23:38:44 +0200

    orderservice update order init

--- server/src/main/java/server/Service/OrderService.java
+++ server/src/main/java/server/Service/OrderService.java
@@ -1,8 +1,14 @@
 package server.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import server.Database.OrderRepository;
+import server.Entity.Book;
 import server.Entity.Order;
+import server.Response.ResponseText;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +17,7 @@ import java.util.Optional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    public static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -33,5 +40,15 @@ public class OrderService {
     public Long deleteOrder(Long id){
         orderRepository.deleteById(id);
         return id;
+    }
+
+    public ResponseEntity<ResponseText> updateOrder(Long id, Order order){
+
+        if(orderRepository.findById(id).isPresent()){
+            //impl
+            return new ResponseEntity<>(new ResponseText("Modification Successful!"), HttpStatus.OK);
+        }
+        LOGGER.info("Order Modified with id : " + id);
+        return new ResponseEntity<>(new ResponseText("Invalid Input!"), HttpStatus.BAD_REQUEST);
     }
 }
