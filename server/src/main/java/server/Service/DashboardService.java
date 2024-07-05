398b87617921b94645b90ab0b85a4f8f6e8f9ae4
Zoli <zoltankaracsonyi91@gmail.com>
2024-07-05 23:52:56 +0200

    dashboard fix

--- server/src/main/java/server/Service/DashboardService.java
+++ server/src/main/java/server/Service/DashboardService.java
@@ -1,15 +1,23 @@
 package server.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import server.Pojo.Dashboard;
 
 @Service
 public class DashboardService {
 
-    private UserService userService;
-    private BookService bookService;
+    @Autowired
+    private final UserService userService;
+    private final BookService bookService;
+
+    public DashboardService(UserService userService, BookService bookService) {
+        this.userService = userService;
+        this.bookService = bookService;
+    }
 
     public Dashboard createDashboard() {
         return new Dashboard(userService.listUsers().size(),bookService.listBooks().size());
     }
+
 }
