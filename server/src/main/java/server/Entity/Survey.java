f516f438e68de46389edb2e2f1f1d734dbfe54cb
Zoli <zoltankaracsonyi91@gmail.com>
2024-07-13 23:58:56 +0200

    Survey entity added

---
+++ server/src/main/java/server/Entity/Survey.java
@@ -0,0 +1,41 @@
+package server.Entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Survey {
+
+    private long id;
+    private String question;
+    private String answer;
+
+    public Survey(long id, String question, String answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
