490bf399977514df589f3332fc595a5dce76be3f
Zoli <zoltankaracsonyi91@gmail.com>
2024-07-15 21:50:22 +0200

    Survey repository init

---
+++ server/src/main/java/server/Database/SurveyRepository.java
@@ -0,0 +1,8 @@
+package server.Database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import server.Entity.Survey;
+
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey, Long> {}
