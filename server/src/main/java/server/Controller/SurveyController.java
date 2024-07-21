46b43ed685e84f82a176151341158b27aba01099
Zoli <zoltankaracsonyi91@gmail.com>
2024-07-21 22:46:44 +0200

    Survey controller create survey

--- server/src/main/java/server/Controller/SurveyController.java
+++ server/src/main/java/server/Controller/SurveyController.java
@@ -1,6 +1,10 @@
 package server.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import server.Entity.Survey;
 import server.Service.SurveyService;
 
 @Controller
@@ -11,6 +15,13 @@ public class SurveyController {
     public SurveyController(SurveyService surveyService) {
         this.surveyService = surveyService;
     }
+
+    @RequestMapping(value = "/createsurvey", method = RequestMethod.POST)
+    public long createSurvey(@RequestBody Survey survey) {
+        return surveyService.createSurvey(survey);
+    }
+
+
 
 
 }
