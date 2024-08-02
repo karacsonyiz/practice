20807f129a4e7a929ecade4e61ebbd54dec63f79
Zoli <zoltankaracsonyi91@gmail.com>
2024-08-02 23:45:30 +0200

    surveys.js loadsurveys

--- server/src/main/resources/static/js/survey.js
+++ server/src/main/resources/static/js/survey.js
@@ -4,5 +4,11 @@ loadSurveys();
 }
 
 function loadSurveys() {
-
+        fetch("/surveys")
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(jsonData) {
+                console.log(jsonData);
+            }).catch(error => console.log(error));
 }
