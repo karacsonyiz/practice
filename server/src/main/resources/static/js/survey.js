3796537b8394b3fc7341bd7c5a01860af19f7ddf
Zoli <zoltankaracsonyi91@gmail.com>
2024-08-03 00:26:25 +0200

    surveys.js table init

--- server/src/main/resources/static/js/survey.js
+++ server/src/main/resources/static/js/survey.js
@@ -10,5 +10,13 @@ function loadSurveys() {
             })
             .then(function(jsonData) {
                 console.log(jsonData);
+                fillSurveyTable(jsonData);
             }).catch(error => console.log(error));
+}
+
+function fillSurveyTable(jsonData) {
+    console.log(jsonData)
+    let surveyRowDiv = document.querySelector("#surveyRowDiv");
+
+    
 }
